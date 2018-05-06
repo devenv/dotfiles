fa85d447227205fbe404e70b5b57c3c521d50527
Boris Churzin <boris@dynamicyield.com>
2018-05-06 13:13:17 +0300

    update

---
+++ bin/kill_all_dockers.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/sh
+
+docker rm -f `docker ps --no-trunc -aq`
+
+if [ "$(uname)" == "Darwin" ]; then
+  XARGS_OPTS=
+  &>/dev/null xargs -r && XARGS_OPTS+="-r"
+  docker images --filter dangling=true -q | xargs ${XARGS_OPTS} docker rmi
+  docker volume ls -qf dangling=true | xargs ${XARGS_OPTS} docker volume rm
+else
+  docker images --filter dangling=true -q | xargs -r docker rmi
+  docker volume ls -qf dangling=true | xargs -r docker volume rm
+fi
+
