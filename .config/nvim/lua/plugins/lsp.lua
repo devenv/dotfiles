2820469abc03a68713a669b5a019c1c59312180b
Boris Churzin <boris@nilus.com>
2024-12-28 15:57:53 +0200

    feat: Update LSP configuration with Supermaven and Aider integration

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -16,55 +16,67 @@ local plugins = {
           require("blink.compat").setup()
         end,
       },
-      {
-        "supermaven-inc/supermaven-nvim",
-        lazy = false,
-        priority = 50,
-        opts = {
-          disable_inline_completion = false,
-          keymaps = {
-            accept_suggestion = "<C-l>",
-            clear_suggestion = "<C-h>",
-            accept_word = "<Tab>",
-          },
-        },
-      },
     },
-    version = "v0.*",
     opts = {
+      enabled = function()
+        local buftype = vim.bo.buftype
+        local filetype = vim.bo.filetype
+
+        if buftype == "prompt" then
+          return false
+        end
+        if
+          vim.tbl_contains({
+            "DressingInput",
+            "TelescopePrompt",
+            "neo-tree",
+            "neo-tree-popup",
+          }, filetype)
+        then
+          return false
+        end
+
+        return true
+      end,
       sources = {
-        default = { "lsp", "path", "luasnip", "buffer" },
-        compat = { "supermaven" },
-      },
-      providers = {
-        supermaven = {
-          name = "Supermaven",
-          module = "blink.cmp.sources.supermaven",
-          score_offset = 500,
-          async = true,
-          timeout_ms = 2000,
-          enabled = function(ctx)
-            return ctx and ctx.manually_triggered_for == "supermaven"
-          end,
+        default = {
+          "lsp",
+          "path",
+          "luasnip",
+          "buffer",
         },
       },
       completion = {
+        ghost_text = {
+          enabled = false,
+        },
         keyword = {
           range = "full",
         },
         trigger = {
-          show_on_keyword = true, -- Enable triggering after any keystroke
-          show_on_trigger_character = true, -- Respect trigger characters
+          show_on_keyword = true,
+          show_on_trigger_character = true,
           show_on_accept_on_trigger_character = true,
           show_on_insert_on_trigger_character = true,
+          show_in_snippet = false,
         },
         list = {
           selection = "preselect",
+          cycle = {
+            from_bottom = false,
+            from_top = false,
+          },
+        },
+        accept = {
+          create_undo_point = true,
+          auto_brackets = {
+            enabled = false,
+          },
         },
         menu = {
           draw = {
             columns = {
-              { "source_icon" }, -- Add source icon column
+              { "source_icon" },
               { "label", "label_description", gap = 1 },
               { "kind_icon", "kind" },
             },
@@ -72,13 +84,12 @@ local plugins = {
               source_icon = {
                 ellipsis = false,
                 text = function(ctx)
-                  -- Map source names to icons
                   local icons = {
-                    supermaven = "󰢱", -- Maven icon
-                    LSP = "󰃖", -- LSP icon
-                    Buffer = "󰈙", -- Buffer icon
-                    Path = "󰉋", -- Folder icon
-                    Snippets = "󰆐", -- Snippet icon
+                    LSP = "󰃖",
+                    Buffer = "󰈙",
+                    Path = "󰉋",
+                    Snippets = "󰆐",
+                    Copilot = "",
                   }
                   return (icons[ctx.source_name] or ctx.source_name) .. " "
                 end,
@@ -91,10 +102,21 @@ local plugins = {
         },
       },
       keymap = {
-        preset = "default",
+        preset = "super-tab",
+        ["<Tab>"] = { "select_and_accept", "snippet_forward", "fallback" },
         ["<CR>"] = { "accept", "fallback" },
       },
     },
+  },
+  {
+    "joshuavial/aider.nvim",
+    lazy = false,
+    config = function()
+      require("aider").setup({
+        auto_manage_context = true,
+        default_bindings = true,
+      })
+    end,
   },
   {
     "neovim/nvim-lspconfig",
@@ -112,28 +134,27 @@ local plugins = {
         preset = "codicons",
         symbol_map = {
           Class = "󰠱",
-          Codeium = "",
+          Codeium = "",
           Color = "󰏘",
-          Copilot = "",
+          Copilot = "",
           Constant = "󰏿",
-          Constructor = "",
-          Enum = "",
-          EnumMember = "",
-          Event = "",
+          Constructor = "",
+          Enum = "",
+          EnumMember = "",
+          Event = "",
           Field = "󰜢",
           File = "󰈙",
           Folder = "󰉋",
           Function = "󰊕",
-          Interface = "",
+          Interface = "",
           Keyword = "󰌋",
           Method = "󰆧",
-          Module = "",
+          Module = "",
           Operator = "󰆕",
           Property = "󰜢",
           Reference = "󰈇",
-          Snippet = "",
+          Snippet = "",
           Struct = "󰙅",
-          Supermaven = "",
           Text = "󰉿",
           TypeParameter = "",
           Unit = "󰑭",
@@ -150,16 +171,12 @@ local plugins = {
       formatters_by_ft = {
         bash = { "beautysh" },
         sql = { "sqlfmt" },
-
         json = { "deno_fmt" },
-
         css = { "prettier" },
         html = { "prettier" },
         javascript = { "prettier" },
         yaml = { "prettier" },
-
         vue = { "prettier" },
-
         lua = { "stylua" },
         python = {
           "ruff_fix",
